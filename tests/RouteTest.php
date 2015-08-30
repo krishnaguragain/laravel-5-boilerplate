d32e8fb0b361fa82b26511ca79591ba2e0ecc54b
Anthony Rappa <rappa819@gmail.com>
2015-08-30 15:28:38 -0400

    Would help if I named the functions correctly.

--- tests/RouteTest.php
+++ tests/RouteTest.php
@@ -8,7 +8,7 @@ class RouteTest extends TestCase {
 	/**
 	 *
      */
-	public function getHome()
+	public function testHome()
 	{
 		$response = $this->call('GET', '/');
 		$this->assertEquals(200, $response->getStatusCode());
@@ -17,7 +17,7 @@ class RouteTest extends TestCase {
 	/**
 	 *
      */
-	public function getMacros()
+	public function testMacros()
 	{
 		$response = $this->call('GET', '/macros');
 		$this->assertEquals(200, $response->getStatusCode());
